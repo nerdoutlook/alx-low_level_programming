94eec422b2712719a8df796ce8e2b1698b874106
nerdoutlook <vincentuyanwanne@gmail.com>
2022-09-09 09:57:21 -0700

    Adding 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @void
+ * Return: program returns 1
+*/
+
+int main(void)
+{
+	printf("and that piece of art is useful\" -Dora Korpar, 2015-10-19,");
+
+	return (0);
+}
