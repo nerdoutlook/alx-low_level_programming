6082d1cebed4c0ddb3d7b37dfad4a47757db516d
nerdoutlook <vincentuyanwanne@gmail.com>
2022-09-09 07:55:26 -0700

    Adding 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print text given to printf
+ * @void
+ *
+ * Return: Returns zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
