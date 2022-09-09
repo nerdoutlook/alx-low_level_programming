4cd79684a42e390d0a44239c7feaa801b06fc750
nerdoutlook <vincentuyanwanne@gmail.com>
2022-09-09 07:20:46 -0700

    Modifying 4-puts.c to reflect defined styling

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - main function
+ * @void
+ * Return: program returns 0
+*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
