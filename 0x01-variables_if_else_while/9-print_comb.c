7436bcee7691284f4eeae67df5eb32c4737766ae
nerdoutlook <vincentuyanwanne@gmail.com>
2022-09-13 16:34:22 +0000

    Modifying 9-print_comb c program file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,6 +5,7 @@ int main()
 	int n = 48;
 	int comma = 44;
 	int new_line = 10;
+	int space = 32;
 
 	
 	while (n <= 57)
@@ -13,6 +14,7 @@ int main()
 		{
 			putchar(n);
 			putchar(comma);
+			putchar(space);
 		}
 
 		if (n == 57)
