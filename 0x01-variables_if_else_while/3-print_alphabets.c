bf4902f53eebff7dfef2e6acfad4d3f30b59c44d
nerdoutlook <vincentuyanwanne@gmail.com>
2022-09-13 19:05:16 +0000

    Fixing Betty styling

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - prints the letters of the alphabets
+ * Lowercase first then uppercase
+ *
+ * Return: Returns 0
+ */
+int main(void)
 {
 	char lowerCase = 'a';
 	char upperCase = 'A';
 	char nl = '\n';
 
-	while ( lowerCase <= 'z')
+	while (lowerCase <= 'z')
 	{
 		putchar(lowerCase);
 		lowerCase++;
 	}
-	while ( upperCase <= 'Z')
+	while (upperCase <= 'Z')
 	{
 		putchar(upperCase);
 		upperCase++;
 	}
 
 	putchar(nl);
-
 	return (0);
 }
