a74ea6a4862e4badf043f4e5c3b26cf057932419
nerdoutlook <vincentuyanwanne@gmail.com>
2022-09-21 03:45:10 +0000

    Adding 100-print_comb3.c & 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - print possible combination
+ * Of two digits, no repitiion.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if (j != 48)
+			{
+				if (i != j && i < j)
+				{
+					putchar(i);
+					putchar(j);
+					if (i > j)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints possible combination
+ * For 3 digit numbers, no repition of
+ * The same digits.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (j != 48 && i != j)
+				{
+					if (i != k && i < j && j < k)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
