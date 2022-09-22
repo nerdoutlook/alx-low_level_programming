6ab95408f4c2687fd9a5aa5e7504cff907f8f570
nerdoutlook <vincentuyanwanne@gmail.com>
2022-09-22 05:18:54 +0000

    Adding 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 -  prints the alphabet
+ * On lowercase on 10 lines.
+ *
+ * Return: Returns nothing.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int x = 0;
+
+	while (x <=9)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		x++;
+	}
+}
