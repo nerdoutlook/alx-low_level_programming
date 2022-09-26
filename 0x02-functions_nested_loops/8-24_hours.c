b6f81ac4fc6666f27f6c0fcf83d7026295eb4a5f
nerdoutlook <vincentuyanwanne@gmail.com>
2022-09-26 00:14:54 +0000

    Adding 8-24_hours and main.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+ #include "main.h"
+
+/** 
+ * jack_bauer - Prints every minute of the day in a 24 Hour period.
+ *
+ * Return: Nothing.
+ */
+void jack_bauer(void)
+{
+	int i,j,k,l;
+		for (i = 48; i <= 50; i++)
+		{
+			for (j = 48; j <= 51; j++)
+			{
+				for (k = 48; k <= 53; k++)
+				{
+					for (l = 48; l <= 57; l++)
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+}
