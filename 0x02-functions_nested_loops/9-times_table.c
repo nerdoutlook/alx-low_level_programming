afeaa5ae65a75bc0c51d2b3d2a2c4886a6dacb67
nerdoutlook <vincentuyanwanne@gmail.com>
2022-09-28 01:22:52 +0000

    Modifying 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,37 +13,24 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (j == 1)
+			if (j == 0 || j == 1)
 			{
 				_putchar((i * j) + '0');
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
-			if ((i * j) <= 9)/** Print single digits in the table*/
+			if ((i * j) <= 9 && j >= 2)
 			{
-				if (j != 1)/**Exclude 0 & 1 of space formatting*/
-				{
-					_putchar(' '); /**Space formatting*/
-					_putchar((i * j) + '0');
-					if (j < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
+				_putchar(' ');
+				_putchar((i * j) + '0');
 			}
 			if ((i * j) >= 10)
 			{
 				_putchar((i * j) / 10 + '0');
 				_putchar((i * j) % 10 + '0');
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 			_putchar('\n');
