54a432a04539d1616333559595883c6bf9ef0e96
nerdoutlook <vincentuyanwanne@gmail.com>
2022-10-06 00:06:52 +0000

    Fixing 100 & 101

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,16 +11,14 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n > 15 || n < 0)
-	{
-		printf("\n");
-	}
 	if (n < 15 && n > 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
+				if (n > 15 || n < 0)
+					break;
 				if (j == 0)
 					printf("%d", (i * j));
 				if (j == 1 && (i * j <= 9))
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints the sum of all multiples of 3 or 5 below 1024
+ *
+ * Return: Nothing
+ */
 int main(void)
 {
 	int i, total;
