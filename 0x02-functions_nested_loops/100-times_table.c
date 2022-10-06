701975fcdda72e009da19dc491ee8974846f21c0
nerdoutlook <vincentuyanwanne@gmail.com>
2022-10-06 01:11:48 +0000

    Fixing 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,14 +11,14 @@ void print_times_table(int n)
 {
 	int i, j;
 
+	if (n == 0)
+		printf("%d\n", n);
 	if (n < 15 && n > 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				if (n == 0)
-					printf("%d\n", n);
 				if (n > 15 || n < 0)
 					break;
 				if (j == 0)
