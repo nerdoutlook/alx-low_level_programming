d8719f0dfb3513fe7a835fcaa0e9a80e16d83f1a
nerdoutlook <vincentuyanwanne@gmail.com>
2022-10-26 22:56:25 +0000

    Modified 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,20 +13,12 @@ char *_strcat(char *dest, char *src)
 	char j;
 
 	len = get_str_length(dest);
-	/*len = 0;
-	while (dest[len] != '\0')
-		len++;
-	*/
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		j = src[i];
 		dest[len + i] = j;
 	}
-
 	len = get_str_length(dest);
-	/*while (dest[len] != '\0')
-		len++;
-	*/
 	dest[len + 1] = '\0';
 	return (dest);
 }
