693d32d6fdc60f6948b07e06ab4e58d9b08e8676
nerdoutlook <vincentuyanwanne@gmail.com>
2022-11-02 08:55:11 +0000

    Test

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,23 +11,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, len;
+	int i;
 	char j;
 
-	i = 0;
-	while (dest[i] != '\0')
-		i++;
-	len = i;
-
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		j = src[i];
-		dest[len + i] = j;
+		dest[i] = j;
 	}
-
-	while (dest[i] != '\0')
-		i++;
-	len = i;
-	dest[len + 1] = '\0';
+	for ( ; i < n; i++)                                                                                                                                                                                      dest[i] = '\0';
 	return (dest);
 }
