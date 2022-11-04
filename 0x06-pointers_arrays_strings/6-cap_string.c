fb187d0e8c69dcbb922c12e4eca74c3c06fb5826
nerdoutlook <vincentuyanwanne@gmail.com>
2022-11-04 03:36:49 +0000

    Betty documentation fix on 6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,12 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' || str[i] == '.' || str[i] == '?' || str[i] == '\n' || str[i] == '\t')
+		if (str[i] == ' ' || str[i] == '.' || str[i] == '?')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] = str[i + 1] - 32;
+		}
+		if (str[i] == '\n' || str[i] == '\t')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i + 1] - 32;
