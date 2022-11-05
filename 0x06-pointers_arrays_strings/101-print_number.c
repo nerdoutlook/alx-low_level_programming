cb4e2feb0a8b93fe10e13bbf739dc4f40f00bf29
nerdoutlook <vincentuyanwanne@gmail.com>
2022-11-05 15:06:22 +0000

    Fixed betty coding style error

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,16 @@
  */
 void print_number(int n)
 {
-	if (n < 0 && n >= -9)
+	if (n < 0 && n <= -9)
 	{
-
+		_putchar(n + '3');
 	}
 
 	if (n >= 0 && n <= 9)
 	{
 		_putchar(n + '0');
 	}
-	
+
 	if (n >= 10 && n < 99)
 	{
 		_putchar(n / 10 + '0');
