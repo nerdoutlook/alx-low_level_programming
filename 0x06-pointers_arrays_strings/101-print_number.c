3994146b7b503326830b5637c73b765dccdca37f
nerdoutlook <vincentuyanwanne@gmail.com>
2022-11-05 14:55:46 +0000

    It works

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_number - prints to stdout any given integer
+ * @n: integer to be printed
+ *
+ * Return: void.
+ */
+void print_number(int n)
+{
+	if (n < 0 && n >= -9)
+	{
+
+	}
+
+	if (n >= 0 && n <= 9)
+	{
+		_putchar(n + '0');
+	}
+	
+	if (n >= 10 && n < 99)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+
+	if (n >= 100  && n <= 999)
+	{
+		_putchar((n / 10) / 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar((n % 10) % 10 + '0');
+	}
+
+	if (n >= 1000 && n <= 9999)
+	{
+		_putchar(((n / 10) / 10) / 10 + '0');
+		_putchar(((n / 10) / 10) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar((n % 10) + '0');
+	}
+}
+
