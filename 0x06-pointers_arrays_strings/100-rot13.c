0ec1ae635f7b1040bc7e04b09a1faca859c3e821
nerdoutlook <vincentuyanwanne@gmail.com>
2022-11-05 12:14:02 +0000

    It fairly works

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,24 +2,31 @@
 
 /**
  * rot13 - Encodes a string ustr[i]ng rot13
- * @str: pointer variable to string to be encoded
+ * @str: pointer variable to string.
  *
  * Return: Encoded string
  */
 char *rot13(char *str)
 {
-	int k;
+	int k, l;
+	char input[] = {'A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J',
+			'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+			'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+			'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+			'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char output[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			'W', 'X', 'Y', 'Z', 'A', 'B','C', 'D', 'E', 'F', 'G',
+		       	'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r',
+			's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c',
+			'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
 	for (k = 0; str[k] != '\0'; k++)
 	{
-		if (str[k] >= 'A' && str[k] <= 'M')
-			str[k] = str[k] + 13;
-		else if (str[k] >= 'N' && str[k] <= 'Z')
-			str[k] = str[k] - 13;
-		else if (str[k] >= 'a' && str[k] <= 'm')
-			str[k] = str[k] + 13;
-		else if (str[k] >= 'n' && str[k] <= 'z')
-			str[k] = str[k] - 13;
+		for (l = 0; l < 52; l++)
+		{
+			if (str[k] == input[l])
+				str[k] = output[l];
+		}
 	}
 	return (str);
 }
