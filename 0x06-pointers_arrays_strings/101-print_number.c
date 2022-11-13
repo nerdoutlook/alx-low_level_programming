a468608baf5f3bd432d594cd0d1846560d133b6d
nerdoutlook <vincentuyanwanne@gmail.com>
2022-11-13 13:04:21 +0000

    Fixed printing to stdout of negative numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,29 +8,26 @@
  */
 void print_number(int n)
 {
-	if (n < 0 && n <= -9)
+	if (n < 0)
 	{
-		_putchar(n + '3');
+		_putchar(45);
+		n = n * -1;
 	}
-
 	if (n >= 0 && n <= 9)
 	{
 		_putchar(n + '0');
 	}
-
 	if (n >= 10 && n < 99)
 	{
 		_putchar(n / 10 + '0');
 		_putchar(n % 10 + '0');
 	}
-
 	if (n >= 100  && n <= 999)
 	{
 		_putchar((n / 10) / 10 + '0');
 		_putchar((n / 10) % 10 + '0');
 		_putchar((n % 10) % 10 + '0');
 	}
-
 	if (n >= 1000 && n <= 9999)
 	{
 		_putchar(((n / 10) / 10) / 10 + '0');
@@ -38,5 +35,7 @@ void print_number(int n)
 		_putchar((n / 10) % 10 + '0');
 		_putchar((n % 10) + '0');
 	}
+
+
 }
 
