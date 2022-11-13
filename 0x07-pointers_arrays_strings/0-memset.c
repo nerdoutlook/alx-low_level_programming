3c181a336a8741c65916649cb381a1bc65d7026a
nerdoutlook <vincentuyanwanne@gmail.com>
2022-11-13 16:04:33 +0000

    Replace while loop with for loop

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,12 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-	while (i <= n)
-	{
-		*s = b;
-		i++;
-		s++;
-	}
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
